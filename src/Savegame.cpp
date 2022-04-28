64216fd88eb64c653aafb36f541262284e4c0dbc
Hieu Nguyen <hehecoi@outlook.com>
2022-04-28 13:54:47 +0700

    Temporary savegame, not build

---
+++ src/Savegame.cpp
@@ -0,0 +1,134 @@
+#include "Savegame.h"
+
+#include "Box.h"
+#include "Hero.h"
+#include "mapgame.h"
+
+Savegame::Savegame() { movesStack = nullptr; }
+
+Savegame::~Savegame() {
+    while (movesStack) {
+        Node* tmp = movesStack;
+        movesStack = movesStack->next;
+        delete tmp;
+    }
+}
+
+void Savegame::push(int data) {
+    Node* newNode = new Node;
+    newNode->data = data;
+    newNode->next = movesStack;
+    movesStack = newNode;
+}
+
+int Savegame::pop() {
+    if (movesStack == nullptr) return 0;
+    int data = movesStack->data;
+    Node* temp = movesStack;
+    movesStack = movesStack->next;
+    delete temp;
+    return data;
+}
+
+void Savegame::recodeMove(int direction) {
+    switch (direction) {
+        case NOT_MOVE:
+            break;
+        case MOVE_UP:
+            push(MOVE_UP);
+            heroY--;
+            break;
+        case MOVE_DOWN:
+            push(MOVE_DOWN);
+            heroY++;
+            break;
+        case MOVE_LEFT:
+            push(MOVE_LEFT);
+            heroX--;
+            break;
+        case MOVE_RIGHT:
+            push(MOVE_RIGHT);
+            heroX++;
+            break;
+    }
+}
+
+void Savegame::undoMove(Hero& hero) {
+    int direction = pop();
+    switch (direction) {
+        case NOT_MOVE:
+            break;
+        case MOVE_UP:
+            direction = MOVE_DOWN;
+            shiftDown(hero);
+            break;
+        case MOVE_DOWN:
+            direction = MOVE_UP;
+            shiftUp(hero);
+            break;
+        case MOVE_LEFT:
+            direction = MOVE_RIGHT;
+            shiftRight(hero);
+            break;
+        case MOVE_RIGHT:
+            direction = MOVE_LEFT;
+            shiftLeft(hero);
+            break;
+    }
+}
+
+void Savegame::shiftUp(Hero& hero) {
+    hero.Move(MOVE_UP);
+    int i;
+    for (i = heroY + 1; i < 20; i++) {
+        if (!Box::layerBox[i][heroX]) {
+            break;
+        }
+    }
+    for (int j = heroY + 1; j < i; j++) {
+        Box::layerBox[j][heroX]->Move(MOVE_UP);
+    }
+    heroY--;
+}
+
+void Savegame::shiftDown(Hero& hero) {
+    hero.Move(MOVE_DOWN);
+    int i;
+    for (i = heroY - 1; i >= 0; i--) {
+        if (!Box::layerBox[i][heroX]) {
+            break;
+        }
+    }
+    for (int j = heroY - 1; j > i; j--) {
+        Box::layerBox[j][heroX]->Move(MOVE_DOWN);
+    }
+    heroY++;
+}
+
+void Savegame::shiftLeft(Hero& hero) {
+    hero.Move(MOVE_LEFT);
+    int i;
+    for (i = heroX + 1; i < 20; i++) {
+        if (!Box::layerBox[heroY][i]) {
+            break;
+        }
+    }
+    for (int j = heroX + 1; j < i; j++) {
+        Box::layerBox[heroY][j]->Move(MOVE_LEFT);
+    }
+    heroX--;
+}
+
+void Savegame::shiftRight(Hero& hero) {
+    hero.Move(MOVE_RIGHT);
+    int i;
+    for (i = heroX - 1; i >= 0; i--) {
+        if (!Box::layerBox[heroY][i]) {
+            break;
+        }
+    }
+    for (int j = heroX - 1; j > i; j--) {
+        Box::layerBox[heroY][j]->Move(MOVE_RIGHT);
+    }
+    heroX++;
+}
---
+++ src/Savegame.h
@@ -0,0 +1,56 @@
+// TODO: Create struct to save position in every step
+// Breakdown: What to save: Moves & Map
+// How: an 2D Map which save position of hero and boxes, and stack to save step
+
+#ifndef SAVE_GAME_H
+#define SAVE_GAME_H
+
+// Struct node to represent in Stack
+struct Node {
+    int data;
+    Node* next;
+};
+
+class Savegame {
+   public:
+    // Width of a block
+    static const int BLOCK_WIDTH = 32;
+
+    // Initialize the stack
+    Savegame();
+    ~Savegame();
+
+    // Get hero position to save
+    void saveHeroPosition(int x, int y) {
+        heroX = x/BLOCK_WIDTH;
+        heroY = y/BLOCK_WIDTH;
+    }
+
+    // Record move
+    void recodeMove(int direction);
+
+    // Undo move
+    void undoMove(Hero& hero);
+
+    // Direction
+    enum { NOT_MOVE, MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT };
+
+   private:
+    // Stack to save step
+    Node* movesStack;
+    // Push to stack
+    void push(int data);
+    // Pop from stack
+    int pop();
+    
+    // Position of Hero in grid
+    int heroX, heroY;
+
+    // Shift position
+    void shiftUp(Hero& hero);
+    void shiftDown(Hero& hero);
+    void shiftLeft(Hero& hero);
+    void shiftRight(Hero& hero);
+};
+
+#endif
