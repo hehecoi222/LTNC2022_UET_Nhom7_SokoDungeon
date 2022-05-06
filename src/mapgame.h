2fdf614976bfd5247e82c007fac2a311940b8915
Andrew-Bu1 <92152909+Andrew-Bu1@users.noreply.github.com>
2022-05-06 19:13:15 +0700

    Update mapgame.h

--- src/mapgame.h
+++ src/mapgame.h
@@ -1,33 +1,43 @@
 #ifndef mapgame_H_INCLUDED
 #define mapgame_H_INCLUDED
-#include<SDL.h>
+#include "find_res.h"
 #include "Texture.h"
-#include<vector>
-#include<utility>
-using namespace std;
+
 class MapGame
 {
-    private:
-        SDL_Rect des0;//destination x and y to render following by map
-        LTexture wall1;//texture of wall
-        LTexture floor;//texture of floor
-        LTexture Goal;//texture of Goal to win the map
-        
-        //bool checkwin;
-        int currentmap;
-        string map;
+public:
+    //The dimensions of the map
+    static const int GRID_BLOCK_WIDTH = 32;
+    MapGame();
+    //load the map
+    void LoadMap();
+    //Load image, position of box, hero
+    void preLoadMap();
+    //Map level0
+    static char** level0;
+    //Change to the Next Map
+    void NextMap();
+    //Change to the previous Map
+    void PrevMap();
+    //Current map of game
+    int currentmap;
+    //name of the level
+    string map;
+    
+private:    
+    //destination to render
+    SDL_Rect des0;
 
+    //Draw the floor 
+    LTexture floor;
 
-    public:
-        //The dimensions of the map
-        static const int GRID_BLOCK_WIDTH = 32;
-        
-        //load the map
-        void LoadMap();
-        void preLoadMap();
-        //Map level0
-        static char** level0;
-        MapGame();
-        ~MapGame();
+    //Draw the wall
+    LTexture wall;
+
+    //Draw goal position of the box to win 
+    LTexture Goal;
+
+    
 };
-#endif
+
+#endif // MAP_H_INCLUDED
