71745518f296a484021e637d4f7ed3fd6e0db65c
Andrew-Bu1 <92152909+Andrew-Bu1@users.noreply.github.com>
2022-05-07 14:45:29 +0700

    Update Game.h

--- src/Game.h
+++ src/Game.h
@@ -5,16 +5,18 @@
 using namespace std;
 //Using SDL, SDL_image, standard IO, and strings
 
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
-#include <SDL_mixer.h>
+#include <SDL2\SDL.h>
+#include <SDL2\SDL_image.h>
+#include <SDL2\SDL_ttf.h>
+#include <SDL2\SDL_mixer.h>
+
 class Game {
    private:
     int cnt = 0;
     bool isRunning = true;
     SDL_Window* gWindow = NULL;
-
+    bool is_Win = false;
+    
 public:
     Game();
     ~Game();
@@ -33,6 +35,15 @@ public:
     static SDL_Renderer* gRenderer;
 
     static TTF_Font* gFont;
+
+    //Sound of Victory
+    static Mix_Music* gVictory;
+    //Soundtrack 
+    static Mix_Music* gMusic;
+    //Sound effect of Hero
+    static Mix_Chunk* gHero;
+    //Sound effect of Box
+    static Mix_Chunk* gBox;
 
     int getScreenW(){return WINDOW_WIDTH;}
     int getScreenH() {return WINDOW_HEIGHT;}
