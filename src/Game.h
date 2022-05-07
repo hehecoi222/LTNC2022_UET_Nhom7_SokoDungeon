1a5523ddd25956a9820109b0381e8cee841b75e7
Andrew-Bu1 <92152909+Andrew-Bu1@users.noreply.github.com>
2022-05-07 16:42:45 +0700

    Update Game.h

--- src/Game.h
+++ src/Game.h
@@ -5,10 +5,10 @@
 using namespace std;
 //Using SDL, SDL_image, standard IO, and strings
 
-#include <SDL2\SDL.h>
-#include <SDL2\SDL_image.h>
-#include <SDL2\SDL_ttf.h>
-#include <SDL2\SDL_mixer.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
 
 class Game {
    private:
